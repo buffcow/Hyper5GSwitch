91265a350bd029a70f4e8691104865a004f5b336
qingyu <qyma.yyy@qq.com>
2023-12-15 11:47:40 +0800

    chore: used git tag as version name and commit count as version code

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,27 @@ plugins {
     alias(libs.plugins.agp.lib) apply false
 }
 
-val defaultAppVerCode by extra(2)
-val defaultAppVerName by extra("1.0.2")
+val gitCommitCount
+    get() = kotlin.runCatching {
+        Runtime.getRuntime().exec("git rev-list --count HEAD").let { process ->
+            process.waitFor()
+            process.inputStream.bufferedReader().readText().trim().toInt()
+        }
+    }.getOrDefault(0)
+
+val gitCommitTag
+    get() = kotlin.runCatching {
+        Runtime.getRuntime().exec("git describe --tags --long").let { process ->
+            process.waitFor()
+            val output = process.inputStream.bufferedReader().readText().trim()
+            val split = output.split('-')
+            if (split.size < 3) throw NullPointerException()
+            "${split[0].filter { it.isDigit() || it == '.' }}.${split[1]}"
+        }
+    }.getOrDefault("1.0.0")
+
+val defaultAppVerCode by extra(gitCommitCount)
+val defaultAppVerName by extra(gitCommitTag)
 val defaultAppPackageName by extra("cn.buffcow.hyper5g")
 
 val androidMinSdkVersion by extra(24)
@@ -23,6 +42,7 @@ val androidCompileSdkVersion by extra(34)
 val androidBuildToolsVersion by extra("34.0.0")
 val androidSourceCompatibility by extra(JavaVersion.VERSION_17)
 val androidTargetCompatibility by extra(JavaVersion.VERSION_17)
+
 
 subprojects {
     plugins.withType(AndroidBasePlugin::class.java) {
